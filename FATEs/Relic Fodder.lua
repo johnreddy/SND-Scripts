f5feefd79cc257d1a0629673a0c4a6b893944096
John 'Skip' Reddy <1750732+johnreddy@users.noreply.github.com>
2025-12-28 12:57:15 -0600

    Update Relic Fodder.lua
    Need quotes

--- FATEs/Relic Fodder.lua
+++ FATEs/Relic Fodder.lua
@@ -12,7 +12,7 @@ configs:
     description: Name of the primary fate macro script.
     default: ""
   NumberToFarm:
-    description: How many of each relicFodder to farm?  Generally atma 1, 5.35 memories: 20, 5.45 memories: 18, demiatma: 6
+    description: "How many of each relicFodder to farm?  Generally atma 1, 5.35 memories: 20, 5.45 memories: 18, demiatma: 6"
     default: 1
   RelicFodderTarget:
     description: Select the relic fodder you want to farm FATEs for.
